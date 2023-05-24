134ef59bb863a756176e1dd25c12cdf9385e9d32
elf <e.feasley@gmail.com>
2023-05-24 11:50:10 -0400

    matches

--- courtscraper/cli.py
+++ courtscraper/cli.py
@@ -5,7 +5,8 @@ from typing import Optional
 import typer
 
 from courtscraper.ny.scrape import run_scraper
-from courtscraper.ny.doccs_foil_text_to_xlsx import txt_to_xlsx
+from courtscraper.ny.gen_matches import gen_matches
+from courtscraper.ny.doccs_foil_text_to_xlsx import gen_xlsx
 from courtscraper import __app_name__, __version__
 
 app = typer.Typer()
@@ -16,11 +17,17 @@ def scrape() -> None:
     print('scraping')
     run_scraper()
 
+@app.command(name="gen_matches")
+def generate_matches() -> None:
+    """Prints matches between the tracker and doccs data"""
+    print('generating...')
+    gen_matches()
+
 @app.command(name="parse-doccs-foil")
 def parse_doccs_foil() -> None:
     """Parse the DOCCS FOIL data."""
     print('parsing')
-    txt_to_xlsx()
+    gen_xlsx()
 
 def _version_callback(value: bool) -> None:
     if value:
@@ -38,4 +45,5 @@ def main(
         is_eager=True,
     )
 ) -> None:
+    """Print basic info"""
     return
---
+++ courtscraper/ny/gen_matches.py
@@ -0,0 +1,88 @@
+"""Utilities to generate potential matches"""
+import pandas as pd
+
+from courtscraper.data_utils.consts import DOCCS_FOIL_XLSX, TRACKER_PATH
+
+def gen_convictions_df():
+    """Generates a dataframe of convictions from the DOCCS list"""
+    doccs_df = pd.read_excel(DOCCS_FOIL_XLSX)
+    convictions = []
+    crimes = ['Most Serious Crime', 'Second Crime', 'Third Crime']
+    counties = ['County of Indictment 1', 'County of Indictment 2', 'County of Indictment 3']
+    for idx, fieldname in enumerate(crimes):
+        for _, line in doccs_df.iterrows():
+            if line[fieldname] == 'MURDER 2ND':
+                convictions.append([
+                    line['DIN'],
+                    line['Name'],
+                    line['Date of Birth'],
+                    line['Ethnicity'],
+                    line[fieldname],
+                    line[counties[idx]],
+                    line['Min Prison Term in Months']
+                ])
+    convs = pd.DataFrame(convictions)
+    convs.columns = ['DIN', 'Name', 'DOB', 'Race', 'Crime', 'County', 'Min Term (Months)']
+
+    convs.Name = convs.Name.apply(lambda x: x.title())
+    convs.Race = convs.Race.apply(lambda x: x.title())
+    convs.County = convs.County.apply(lambda x: x.title())
+    convs.County = convs.County.apply(lambda x: 'St. Lawrence' if x == 'St Lawrence' else x)
+    convs['YOB'] = convs.DOB.apply(lambda x: int(str(x)[:4]))
+    convs.Race = convs.Race.apply(lambda x: x if x in
+                                  ['Black', 'White', 'Hispanic', 'Asian']
+                                   else 'Other-Unknown')
+    convs = convs.drop_duplicates()
+    return convs
+
+def gen_matches():
+    """Generates exact matches between the tracker data - 
+    skipping over the ones that have already been matched"""
+
+    # Generate dataframe of convictions from the DIN liist
+    convs = gen_convictions_df()
+    tracker = pd.read_excel(TRACKER_PATH, sheet_name=1)
+    tracker['AutoName'] = tracker['Name']
+
+
+    for _, row in tracker.iterrows():
+        if row['AutoName'] and str(row['AutoName']) != 'nan':
+            continue
+        if row['Crime Year'] != 'Unknown' and row['Crime Year'] >= 2020:
+            continue
+        match_convs = convs[convs['County'].apply(lambda x: x == row['Disposition County'])]
+        if str(row['Min Prison Term in Months']) in ['nan', 'Unknown']:
+            match_convs = match_convs[
+                match_convs['Min Term (Months)'].apply(
+                    lambda x: str(x) == 'nan')]
+        else:
+            match_convs = match_convs[
+                match_convs['Min Term (Months)'].apply(
+                    lambda x: str(x) != 'nan' and int(x) ==
+                        int(row['Min Prison Term in Months']))]
+
+        if match_convs.empty:
+            continue
+        match_convs = match_convs[
+            match_convs['Race'].apply(
+                lambda x: x == row['Race/Ethnicity of Arrestee'])]
+        if match_convs.empty:
+            continue
+        if row['Crime Year'] != 'Unknown':
+            potential_yobs = [(row['Crime Year'] - row['Age at Crime']),
+                            (row['Crime Year'] - row['Age at Crime']) - 1]
+            match_convs = match_convs[
+                match_convs['YOB'].apply(
+                    lambda x: x in potential_yobs)]
+        if match_convs.empty:
+            continue
+        match_convs = match_convs[match_convs['DIN'].apply(
+                lambda x: int(x[:2]) >= int(str(row['Arrest Year'])[2:])
+            )]
+
+        if not match_convs.empty:
+            print('\n\n----------------------')
+            print(f"Original Number: {row['Original Number']} Crime Year: {row['Crime Year']}")
+            print(f"\tAge at Arrest: {row['Age at Crime']}")
+            print(f"\t Arrest Year: {row['Arrest Year']}")
+            print(match_convs[['DIN', 'Name', 'DOB', 'Race', 'County', 'Min Term (Months)']])
